676a4d8dcb9e6e65aff533eb6ed5a42137ec9b5f
Snake Layer <snakelayer@gmail.com>
2017-06-01 02:19:19 -0700

    refactor blob data structure to separate file

---
+++ owbot/overwatch/blob.go
@@ -0,0 +1,95 @@
+package overwatch
+
+import (
+	"fmt"
+)
+
+// Top level response to a u/<battle-tag>/blob request
+type BlobResponse struct {
+	KR *RegionBlob `json:"kr"`
+	EU *RegionBlob `json:"eu"`
+	US *RegionBlob `json:"us"`
+}
+
+type RegionBlob struct {
+	Heroes struct {
+		Stats struct {
+			Competitive *AllHeroStats `json:"competitive"`
+			// Quickplay is ignored
+		} `json:"stats"`
+	} `json:"heroes"`
+	Stats struct {
+		Competitive *UserStats `json:"competitive"`
+	} `json:"stats"`
+}
+
+type UserStats struct {
+	BattleTag string
+	Region    string
+
+	OverallStats struct {
+		CompRank int     `json:"comprank"`
+		Games    int     `json:"games"`
+		Level    int     `json:"level"`
+		Losses   int     `json:"losses"`
+		Prestige int     `json:"prestige"`
+		Wins     int     `json:"wins"`
+		WinRate  float32 `json:"win_Rate"`
+	} `json:"overall_stats"`
+	GameStats struct {
+		Deaths       float32 `json:"deaths"`
+		Eliminations float32 `json:"eliminations"`
+		SoloKills    float32 `json:"solo_kills"`
+		KPD          float32 `json:"kpd"`
+		TimePlayed   float32 `json:"time_played"`
+		Medals       float32 `json:"medals"`
+		MedalsGold   float32 `json:"medals_gold"`
+		MedalsSilver float32 `json:"medals_silver"`
+		MedalsBronze float32 `json:"medals_bronze"`
+	} `json:"game_stats"`
+}
+
+func (userStats UserStats) String() string {
+	return fmt.Sprintf("{BattleTag:%v OverallStats:%v}", userStats.BattleTag, userStats.OverallStats)
+}
+
+type AllHeroStats struct {
+	Ana        *HeroStruct `json:"ana"`
+	Bastion    *HeroStruct `json:"bastion"`
+	Dva        *HeroStruct `json:"dva"`
+	Genji      *HeroStruct `json:"genji"`
+	Hanzo      *HeroStruct `json:"hanzo"`
+	Junkrat    *HeroStruct `json:"junkrat"`
+	Lucio      *HeroStruct `json:"lucio"`
+	Mccree     *HeroStruct `json:"mccree"`
+	Mei        *HeroStruct `json:"mei"`
+	Mercy      *HeroStruct `json:"mercy"`
+	Orisa      *HeroStruct `json:"orisa"`
+	Pharah     *HeroStruct `json:"pharah"`
+	Reaper     *HeroStruct `json:"reaper"`
+	Reinhardt  *HeroStruct `json:"reinhardt"`
+	Roadhog    *HeroStruct `json:"roadhog"`
+	Soldier76  *HeroStruct `json:"soldier76"`
+	Sombra     *HeroStruct `json:"sombra"`
+	Symmetra   *HeroStruct `json:"symmetra"`
+	Torbjorn   *HeroStruct `json:"torbjorn"`
+	Tracer     *HeroStruct `json:"tracer"`
+	Widowmaker *HeroStruct `json:"widowmaker"`
+	Winston    *HeroStruct `json:"winston"`
+	Zarya      *HeroStruct `json:"zarya"`
+	Zenyatta   *HeroStruct `json:"zenyatta"`
+}
+
+type HeroStruct struct {
+	// AverageStats is ignored
+	GeneralStats struct {
+		GamesLost   float32 `json:"games_lost"`
+		GamesPlayed float32 `json:"games_played"`
+		GamesWon    float32 `json:"games_won"`
+	} `json:"general_stats"`
+	// HeroStats is ignored
+}
+
+func (heroStruct HeroStruct) String() string {
+	return fmt.Sprintf("{Played:%v Won:%v Lost:%v}", heroStruct.GeneralStats.GamesPlayed, heroStruct.GeneralStats.GamesWon, heroStruct.GeneralStats.GamesLost)
+}
